4ff8d3b5a1911a69ebf13943e379bc2bff440ffe
Prajwal S Belagavi <prajwalpspbelagavi@gmail.com>
2020-04-16 01:06:23 +0530

    using animated widget

---
+++ lib/animated_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimationWidgetPage extends StatefulWidget {
+  @override
+  _AnimationWidgetPageState createState() => _AnimationWidgetPageState();
+}
+
+class _AnimationWidgetPageState extends State<AnimationWidgetPage> with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        duration: Duration(seconds: 5),
+        vsync: this
+    );
+
+    final curvedAnimation = CurvedAnimation(
+        parent: animationController,
+        curve: Curves.bounceIn,
+        reverseCurve: Curves.easeOut
+    );
+
+    animation = Tween<double>(
+        begin: 0,
+        end: 2* math.pi
+    ).animate(curvedAnimation)
+      /*..addListener(() {
+        setState(() {});
+      })*/
+      ..addStatusListener((status) {
+        if(status == AnimationStatus.completed) {
+          animationController.reverse();
+        } else if(status == AnimationStatus.dismissed) {
+          animationController.forward();
+        }
+      });
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: IrisImage(
+          animation: animation,
+        )
+    );
+  }
+}
+
+class IrisImage extends AnimatedWidget {
+
+  IrisImage({
+    @required Animation<double> animation
+  }) : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = super.listenable as Animation<double>;
+
+    return Transform.rotate(
+      angle: animation.value,
+      child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20),
+          child: Image.asset(
+            'assets/fb_iris.png',
+          )
+      ),
+    );
+  }
+}
+
